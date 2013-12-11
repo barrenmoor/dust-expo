a53c7aaf204650fd8520ec020a0fa02c741f8f10
barrenmoor <rajagopalk@gmail.com>
2013-12-11 17:09:56 +0530

    compiled templates bundled into a single js file.

--- src/main/webapp/scripts/compile.js
+++ src/main/webapp/scripts/compile.js
@@ -4,18 +4,18 @@ var compiler = {
 	items : [{
 		template : 'booklist.dust',
 		name : 'booklist',
-		compiled : '_booklist.js'
+		compiled : '_compiled_template.js'
 	}, {
 		template : 'addedit.dust',
 		name : 'addedit',
-		compiled : '_addedit.js'
+		compiled : '_compiled_template.js'
 	}],
 
 	doCompile : function (template, name, compiledTemplate) {
 		fs.readFile(template, 'utf8', function(err, data) {
 			if(err) throw err;
 			var compiled = dust.compile(data, name);
-			fs.writeFile(compiledTemplate, compiled, function(err) {
+			fs.appendFile(compiledTemplate, compiled + "\r\n", function(err) {
 				if(err) {
 					console.log(err);
 				} else {
@@ -23,7 +23,18 @@ var compiler = {
 				}
 			});
 		});
+	},
+
+	clean : function(compiledTemplate) {
+		if(fs.existsSync(compiledTemplate)) {
+			console.log(compiledTemplate + " exists. Now removing");
+			fs.unlinkSync(compiledTemplate);
+		}
 	}
+};
+
+for(var i = 0; i < compiler.items.length; i++) {
+	compiler.clean(compiler.items[i].compiled);
 };
 
 for(var i = 0; i < compiler.items.length; i++) {
