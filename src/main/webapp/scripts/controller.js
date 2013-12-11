d113405c4f982dd1f14221f026754a4a81fa1791
rajagkri <rajagkri@cisco.com>
2013-12-11 16:10:06 +0530

    all CRUD implemented

--- src/main/webapp/scripts/controller.js
+++ src/main/webapp/scripts/controller.js
@@ -1,5 +1,14 @@
+var listBooks = function() {
+	$("#booksMain").empty();
+	$.get("rest/books", function (data, status) {
+		dust.render("booklist", data, function(err, out) {
+			$("#booksMain").append(out);
+		});
+	}, "json");
+};
+
 var deleteBook = function() {
-	var id = $("input[name=delete]:checked").val();
+	var id = $("input[name=selectbook]:checked").val();
 	
 	if(!id) {
 		return;
@@ -22,9 +31,58 @@ var deleteBook = function() {
 };
 
 var editBook = function() {
-	var id = $("input[name=delete]:checked").val();
+	var id = $("input[name=selectbook]:checked").val();
 	
 	if(!id) {
 		return;
 	}
+	
+	$("#booksMain").empty();
+	$.get("rest/books/" + id, function (data, status) {
+		dust.render("addedit", data, function(err, out) {
+			$("#booksMain").append(out);
+		});
+	}, "json");
+};
+
+var createBook = function() {
+	$("#booksMain").empty();
+	dust.render("addedit", {}, function(err, out) {
+		$("#booksMain").append(out);
+	});
+};
+
+var saveBook = function() {
+	var id = $("#id").val();
+	var data = {
+		name : $("#name").val(),
+		author : $("#author").val()
+	};
+	
+	var url = "rest/books/";
+	var op = "POST";
+	if(id) {
+		op = "PUT";
+		url = url + id;
+	}
+	
+	$.ajax({
+		url : url,
+		type : op,
+		data : JSON.stringify(data),
+		dataType : "json",
+		contentType : "application/json",
+		success : function (result) {
+			listBooks();
+		},
+		error : function (jqXHR, textStatus, errorThrown) {
+			console.log(jqXHR);
+			console.log(errorThrown);
+			console.log("textStatus: " + textStatus);
+		}
+	});
+};
+
+var cancel = function () {
+	listBooks();
 };
